51747d081fb8c7cc58a04cbf914aeceabaa0bb0f
serraguti <pacoserranox@gmail.com>
2025-06-23 16:10:32 +0200

    data files

--- main.py
+++ main.py
@@ -4,7 +4,20 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+#Agregamos la librería para utilizar BaseModel
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Dato(BaseModel):
+    nombre: str
+    cantidad: int
+
+#Creamos un metodo POST/PUT que recibirá el objeto Dato
+@app.put("/put")
+def updateDato(dato: Dato):
+    return {"nombre recibido": dato.nombre, "cantidad": dato.cantidad}
 
 #DEBEMOS DECORAR EL METODO CON UN TIPO DE METODO Y UN ENDPOINT 
 #DE ACCESO PARA EL API
@@ -36,6 +49,28 @@ def dameSaludito(nombre: str
 @app.get("/numeros")
 def dameNumeros():
     listaNumeros = [45, 55, 22, 10, 14]
-    return {"numeros": listaNumeros}
+    #Vamos a crear un objeto diccionario para cada número
+    # Cada objeto que tengamos, lo almacenaremos en la colección de salida
+    salida = []
+    for num in listaNumeros:
+        #Creamos un diccionario para cada numero
+        elemento = {"numero": num}
+        codificado = jsonable_encoder(elemento)
+        salida.append(codificado) 
+    return {"numeros": salida}
+
+@app.get("/nombres")
+def dameNombres():
+    listaNombres = ["Adrian", "Lucia", "Manuel", "Antonia"]
+    #Vamos a crear un objeto diccionario para cada número
+    # Cada objeto que tengamos, lo almacenaremos en la colección de salida
+    salida = []
+    for name in listaNombres:
+        #Creamos un diccionario para cada numero
+        elemento = {"name": name}
+        codificado = jsonable_encoder(elemento)
+        salida.append(codificado) 
+    return {"nombres": salida}
+
 
     
